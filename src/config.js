118de49f5954bec136670e30566d95f7121519dd
Hanna Amador <152832888+Hannaaa47@users.noreply.github.com>
2026-03-21 06:27:31 +0000

    ..

--- src/config.js
+++ src/config.js
@@ -9,97 +9,96 @@
  * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
  */
 let config = {
-	address: "localhost",	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
-	port: 8080,
-	basePath: "/",	// The URL path where MagicMirrorÂ² is hosted. If you are using a Reverse proxy
-									// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
-									// or add a specific IPv4 of 192.168.1.5 :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+    address: "localhost",	// Address to listen on, can be:
+                            // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+                            // - another specific IPv4/6 to listen on a specific interface
+                            // - "0.0.0.0", "::" to listen on any interface
+                            // Default, when address config is left out or empty, is "localhost"
+    port: 8080,
+    basePath: "/",	// The URL path where MagicMirrorÂ² is hosted. If you are using a Reverse proxy
+                                    // you must set the sub path here. basePath must end with a /
+    ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
+                                    // or add a specific IPv4 of 192.168.1.5 :
+                                    // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+                                    // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+                                    // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
-	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
-	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+    useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
+    httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
+    httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
 
-	language: "spa", // cambio el idioma a español
+    language: "spa", // cambio el idioma a español
 
-	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
-	timeFormat: 12, // cambio el formato de horas de 24 a 12 
-	units: "metric",
+    logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+    timeFormat: 12, // cambio el formato de horas de 24 a 12 
+    units: "metric",
 
-	modules: [
-		{
-			module: "alert",
-			config: {
-				effect: exploader,
-				alert_effect: bouncyflip,
-				welcome_message: "Hola bb"
-			}
-		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: "clock",
-			position: "top_left", // This can be any of the regions.
-			config: {
-				timeFormat: 12,
-				timezone: "America/Mazatlan",
-				displaySeconds: false,
-				showWeek: true,
-				showMoonTimes: phase,
-				dateFormat: "dddd, Do MMMM YYYY"
-			},
-		},
-		{
-			module: "calendar",
-			header: "Calendario",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						
-						fetchInterval: 24 * 60 * 60 * 1000,
-						dateFormat: "DD/MM/YY",
-						symbol: "calendar-check",
-						url: "https://calendar.google.com/calendar/ical/hanna.amadorholamundo%40gmail.com/public/basic.ics"
-					}
-				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third",
-			config: {
-				remoteFile: 'https://github.com/Hannaaa47/MagicMirror/blob/main/src/compliments.json',
-				compliments: {
-					"....-03-20": [
-					"Si funciona!"
-					],
-					rain: [
-					"Rainy days I'm thinkin' 'bout you"
-					]
-				}
-			}
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "current",
-				lat: 24.8091,
-        		lon: -107.3940
-			}
-		}
-	]
+    modules: [
+        {
+            module: "alert",
+            config: {
+                welcome_message: "Hola bb"
+            }
+        },
+        {
+            module: "updatenotification",
+            position: "top_bar"
+        },
+        {
+            module: "clock",
+            position: "top_left", 
+            config: {
+                timeFormat: 12,
+                timezone: "America/Mazatlan",
+                displaySeconds: false,
+                showWeek: "short",
+                showMoonTimes: "phase",
+                dateFormat: "dddd, Do MMMM YYYY"
+            },
+        },
+        {
+            module: "calendar",
+            header: "Calendario",
+            position: "top_left",
+            config: {
+                calendars: [
+                    {
+                        
+                        fetchInterval: 24 * 60 * 60 * 1000,
+                        dateFormat: "DD/MM/YY",
+                        symbol: "calendar-check",
+                        url: "https://calendar.google.com/calendar/ical/hanna.amadorholamundo%40gmail.com/public/basic.ics"
+                    }
+                ]
+            }
+        },
+        {
+            module: "compliments",
+            position: "lower_third",
+            config: {
+                remoteFile: 'https://github.com/Hannaaa47/MagicMirror/blob/main/src/compliments.json',
+                compliments: {
+                    "....-01-01": [
+                        "FELIZ AÑO NUEVO WUJUU!"
+                    ],
+                    "....-03-20": [
+                        "funciono!"
+                    ]
+                }
+            }
+        },
+        {
+            module: "weather",
+            position: "top_right",
+            config: {
+                weatherProvider: "openmeteo",
+                type: "current",
+                onlyTemp: true,
+                lat: 24.8091,
+                lon: -107.3940
+            }
+        }
+    ]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
